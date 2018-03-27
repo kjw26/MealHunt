32de4c197e814fe3217ea7ef321962bfb4fcfb14
Emil <em235@njit.edu>
2018-03-27 18:47:31 -0400

    landing page; also includes nav bar setup;

---
+++ app/src/main/java/com/mealhunt/mealhunt/LpActivity.java
@@ -0,0 +1,14 @@
+package com.mealhunt.mealhunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lp);
+    }
+
+}
