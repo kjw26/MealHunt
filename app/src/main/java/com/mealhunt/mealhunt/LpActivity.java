69b3321761c076c3e9edd95efbcc64863753aec1
Emil <em235@njit.edu>
2018-04-25 04:13:35 -0400

    contains added views/layouts

--- app/src/main/java/com/mealhunt/mealhunt/LpActivity.java
+++ app/src/main/java/com/mealhunt/mealhunt/LpActivity.java
@@ -1,14 +1,79 @@
 package com.mealhunt.mealhunt;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class LpActivity extends AppCompatActivity {
+public class LpActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lp);
+        setContentView(R.layout.activity_landing);
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        Button button = (Button) findViewById(R.id.button13);
+        if (mNavigationView != null) {
+            mNavigationView.setNavigationItemSelectedListener(this);
+        }
+
+
+}
+    public void faceIn(View view)
+    {
+        Intent myIntent = new Intent(LpActivity.this, MainActivity.class);
+        LpActivity.this.startActivity(myIntent);
+    }
+    public void homeGo(View view)
+    {
+        setContentView(R.layout.activity_landing);
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        if (mNavigationView != null) {
+            mNavigationView.setNavigationItemSelectedListener(this);
+        }
     }
 
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            setContentView(R.layout.activity_landing);
+            NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+
+            if (mNavigationView != null) {
+                mNavigationView.setNavigationItemSelectedListener(this);
+            }
+        }
+        if (id == R.id.nav_invite) {
+            setContentView(R.layout.activity_add_members);
+            NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+
+            if (mNavigationView != null) {
+                mNavigationView.setNavigationItemSelectedListener(this);
+            }
+        }
+        if (id == R.id.nav_groups) {
+            setContentView(R.layout.activity_group_page);
+            NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+
+            if (mNavigationView != null) {
+                mNavigationView.setNavigationItemSelectedListener(this);
+            }
+        }
+        if (id == R.id.nav_about) {
+            setContentView(R.layout.activity_about);
+            NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+
+            if (mNavigationView != null) {
+                mNavigationView.setNavigationItemSelectedListener(this);
+            }
+        }
+        return false;
+    }
+    
 }
