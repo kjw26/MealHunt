bafe53b9a26905e9cb8a0adc23c0e6cef527538d
Emil <em235@njit.edu>
2018-05-01 14:15:36 -0400

    contains added views/layouts fixed nav bar and landing page button issues

--- app/src/main/java/com/mealhunt/mealhunt/group_page.java
+++ app/src/main/java/com/mealhunt/mealhunt/group_page.java
@@ -1,22 +1,26 @@
 package com.mealhunt.mealhunt;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class group_page extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+public class group_page extends LpActivity implements MyRecyclerViewAdapter.ItemClickListener, NavigationView.OnNavigationItemSelectedListener {
 
     MyRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_page);
+        setContentView(R.layout.activity_landing3);
 
         // data to populate the RecyclerView with
         ArrayList<String> groupName = new ArrayList<>();
@@ -29,15 +33,52 @@ public class group_page extends AppCompatActivity implements MyRecyclerViewAdapt
         // set up the RecyclerView
         RecyclerView recyclerView = findViewById(R.id.groupNa);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new MyRecyclerViewAdapter (this, groupName);
+        adapter = new MyRecyclerViewAdapter(this, groupName);
         adapter.setClickListener(this);
         recyclerView.setAdapter(adapter);
         recyclerView.addItemDecoration(new VerticalSpaceItemDecoration(107));
         recyclerView.addItemDecoration(new HorizontalSpaceItemDecoration(100));
+
+        NavigationView lNavigationView = (NavigationView) findViewById(R.id.nav_view3);
+
+        if (lNavigationView != null) {
+            lNavigationView.setNavigationItemSelectedListener(this);
+        }
+
     }
 
-    @Override
-    public void onItemClick(View view, int position) {
-        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + (position + 1), Toast.LENGTH_SHORT).show();
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            Intent myIntent = new Intent(group_page.this, LpActivity.class);
+            group_page.this.startActivity(myIntent);
+        }
+        if (id == R.id.nav_invite) {
+            Intent myIntent = new Intent(group_page.this, invite_activity.class);
+            group_page.this.startActivity(myIntent);
+        }
+        if (id == R.id.nav_groups) {
+            Intent myIntent = new Intent(group_page.this, group_page.class);
+            group_page.this.startActivity(myIntent);
+
+
+        }
+        if (id == R.id.nav_about) {
+            Intent myIntent = new Intent(group_page.this, about_page.class);
+            group_page.this.startActivity(myIntent);
+        }
+        return false;
     }
-}
+
+        @Override
+        public void onItemClick (View view,int position){
+            Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + (position + 1), Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPointerCaptureChanged ( boolean hasCapture){
+
+        }
+    }
--- app/src/main/java/com/mealhunt/mealhunt/invite_activity.java
+++ app/src/main/java/com/mealhunt/mealhunt/invite_activity.java
@@ -1,36 +1,39 @@
 package com.mealhunt.mealhunt;
 
 import android.content.Intent;
-import android.support.design.widget.NavigationView;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class LpActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class invite_activity extends LpActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing);
-        NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        setContentView(R.layout.activity_landing4);
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_view4);
         Button button = (Button) findViewById(R.id.button13);
         if (mNavigationView != null) {
             mNavigationView.setNavigationItemSelectedListener(this);
         }
 
 
-}
-    public void faceIn(View view)
-    {
-        Intent myIntent = new Intent(LpActivity.this, MainActivity.class);
-        LpActivity.this.startActivity(myIntent);
     }
-    public void homeGo(View view)
-    {
+
+    public void faceIn(View view) {
+        Intent myIntent = new Intent(invite_activity.this, MainActivity.class);
+        invite_activity.this.startActivity(myIntent);
+    }
+
+    public void homeGo(View view) {
         setContentView(R.layout.activity_landing);
-        NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_view2);
 
         if (mNavigationView != null) {
             mNavigationView.setNavigationItemSelectedListener(this);
@@ -50,20 +53,19 @@ public class LpActivity extends AppCompatActivity implements NavigationView.OnNa
             }
         }
         if (id == R.id.nav_invite) {
-            Intent myIntent = new Intent(LpActivity.this, invite_activity.class);
-            LpActivity.this.startActivity(myIntent);
+            Intent myIntent = new Intent(invite_activity.this, invite_activity.class);
+            invite_activity.this.startActivity(myIntent);
         }
         if (id == R.id.nav_groups) {
-            Intent myIntent = new Intent(LpActivity.this, group_page.class);
-            LpActivity.this.startActivity(myIntent);
+            Intent myIntent = new Intent(invite_activity.this, group_page.class);
+            invite_activity.this.startActivity(myIntent);
 
 
         }
         if (id == R.id.nav_about) {
-            Intent myIntent = new Intent(LpActivity.this, about_page.class);
-            LpActivity.this.startActivity(myIntent);
+            Intent myIntent = new Intent(invite_activity.this, about_page.class);
+            invite_activity.this.startActivity(myIntent);
         }
         return false;
     }
-    
 }
